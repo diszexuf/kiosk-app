5a713f8b690ad055dff7680ce90f7c57d6dccd87
diszexuf <diszexuf@yandex.ru>
2026-04-03 19:55:07 +0500

    chore: remove cached files, apply .gitignore

---
+++ kiosk-app-backend/src/main/java/com/diszexuf/kioskappbackend/KioskAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.diszexuf.kioskappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KioskAppBackendApplication.class, args);
+    }
+
+}
---
+++ kiosk-app-backend/src/test/java/com/diszexuf/kioskappbackend/KioskAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diszexuf.kioskappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
